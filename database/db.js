07b1e73be574cdc9c07d8e5e4e919d88ea559bb5
lesetro <leandrosebastiantroncoso@gmail.com>
2025-06-16 17:01:22 -0300

    backup

--- database/db.js
+++ database/db.js
@@ -1,15 +1,14 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.MYSQL_URL, {
+const sequelize = new Sequelize(process.env.MYSQL_URL || {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   dialect: 'mysql',
-  retry: { max: 3 },
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+  retry: { max: 3 } 
 });
 const db = {
   Sequelize,
