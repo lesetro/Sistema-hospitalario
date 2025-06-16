37672cd48821dba78fd1333a2f49408f22439ac5
lesetro <leandrosebastiantroncoso@gmail.com>
2025-06-16 16:12:52 -0300

    base

--- database/db.js
+++ database/db.js
@@ -1,25 +1,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: 3306, 
-    dialect: 'mariadb',
-    dialectOptions: {
-      connectTimeout: 30000
-    },
-    logging: false,
-    define: {
-      timestamps: true,
-      underscored: true
-    }
-  }
-);
-
+const sequelize = new Sequelize(process.env.MYSQL_URL || {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  dialect: 'mysql',
+  retry: { max: 3 } 
+});
 const db = {
   Sequelize,
   sequelize,
