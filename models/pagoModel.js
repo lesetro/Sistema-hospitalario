5c8b4f702257681180591d359e0f3e07f96033bf
lesetro <leandrosebastiantroncoso@gmail.com>
2025-06-17 01:02:14 -0300

    sincronizar

--- models/pagoModel.js
+++ models/pagoModel.js
@@ -8,7 +8,11 @@ module.exports = (sequelize, DataTypes) => {
     fecha: { type: DataTypes.DATE, allowNull: false },
     metodo: { type: DataTypes.ENUM('Efectivo', 'Tarjeta', 'Transferencia', 'Obra Social'), allowNull: false },
     estado: { type: DataTypes.ENUM('Pendiente', 'Completado', 'Rechazado'), defaultValue: 'Pendiente' },
-    motivo_rechazo: { type: DataTypes.TEXT, allowNull: true }
+    motivo_rechazo: { type: DataTypes.TEXT, allowNull: true },
+    tipo_pago: {
+  type: DataTypes.STRING,
+  allowNull: true // o false según tus requisitos
+}
 
   }, {
     tableName: 'pagos',
